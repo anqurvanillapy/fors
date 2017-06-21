558471b51903d485db2741e05be4fd80fd7d85b9
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-06-21 21:34:47 +0800

    wow patterns

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,28 @@ use std::process;
 use std::path::Path;
 use std::fs::File;
 use std::io::BufReader;
+use std::io::Error;
 use std::io::prelude::*;
+
+fn log_fatal(e: Error) {
+    println!("{}", e);
+    process::exit(1);
+}
 
 fn main() {
     if let Some(arg1) = env::args().nth(1) {
         let fpath = Path::new(&arg1);
-        if let Ok(file) = File::open(&fpath) {
-            let mut buf_reader = BufReader::new(file);
-            let mut src = String::new();
+        match  File::open(&fpath) {
+            Ok(file) => {
+                let mut buf_reader = BufReader::new(file);
+                let mut src = String::new();
 
-            if let Ok(_) = buf_reader.read_to_string(&mut src) {
-                println!("src={}", src);
-            }
+                match buf_reader.read_to_string(&mut src) {
+                    Ok(_) => println!("src={}", src),
+                    Err(e) => log_fatal(e)
+                }
+            },
+            Err(e) => log_fatal(e)
         }
     } else {
         println!("Usage: fors filename");
